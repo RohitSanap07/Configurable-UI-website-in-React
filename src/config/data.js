bf3d441ea9e7cde1a05360113db8a5d5a0f4b4af
RohitSanap07 <rohitsanap177@gmail.com>
2024-09-07 01:34:59 +0530

    data file added

---
+++ src/config/data.js
@@ -0,0 +1,68 @@
+const MAIN_TITLE = "Tech Mart💻"
+const PRODUCT_LIST = "Shop the Latest and Greatest in Tech"
+
+
+const PRODUCTS = [
+    {
+      imgUrl: "https://img.freepik.com/free-vector/laptop-with-rocket_23-2147503371.jpg?t=st=1725169290~exp=1725172890~hmac=e52935d15afc38161a196f189fc73e852b8044640301c22113c1e2c2402a4fb5&w=740",
+      title: "Laptop",
+      description: "HP Laptop AMD Ryzen 3 Quad Core 5300U - (8 GB/512 GB SSD/Windows 11 Home)",
+      isSmall: true,
+      price: 40000
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-photo/device-protected-by-cyber-security_23-2149270841.jpg?t=st=1725169603~exp=1725173203~hmac=23293d0faf4842d87a1eb02d212ddfd7d1c11a4c9ac363feac3df0b5cc3178ec&w=740",
+      title: "Smart Watch",
+      description: "PLAYFIT STRENGTH, Rugged & Sporty with IPS Display, Bluetooth Calling, 2W EBEL Speaker.",
+      isSmall: true,
+      price: 1999
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-vector/realistic-wireless-earphones-illustration_1284-20763.jpg?t=st=1725169817~exp=1725173417~hmac=43c8437f62d30f2b88a7060a0c49a7634561f81a95b8c05986f05686ae193055&w=740",
+      title: "Headphone",
+      description: "ZEBRONICS Zeb- Thunder, With 60H Backup, Gaming Mode, ENC, AUX, Dual Pairing Bluetooth",
+      isSmall: false,
+      price: 3499
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-vector/smartphone-realistic-design_52683-29766.jpg?t=st=1725169966~exp=1725173566~hmac=e512b331fac91572302d638801494a94c08b7076615b9d069c794e7f4083f80e&w=740",
+      title: "Mobile",
+      description: "Vivo T1 44W (Starry Sky, 128 GB)  (6 GB RAM).",
+      isSmall: true,
+      price: 14999
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-photo/man-watching-his-favorite-movie-tv_23-2149047399.jpg?t=st=1725170144~exp=1725173744~hmac=1f9065a0d13bfc36741353e92306fb5e1a01e0c15556bf58d7b4081443af7419&w=996",
+      title: "Smart TV",
+      description: "Mi X Series(65 inch) Ultra HD (4K) LED Smart Google TV with 4K Dolby Vision & Dolby Audio.",
+      isSmall: true,
+      price: 45000
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-vector/loud-speakers-isolated-illustration_73621-990.jpg?t=st=1725170459~exp=1725174059~hmac=f28095f70652c9e5866afc7fd7e19e63390bdae76addaae3062b45c2ed0ff15f&w=740",
+      title: "Speaker",
+      description: "Blaupunkt TS120 BASS 140 W Bluetooth Tower Speaker.",
+      isSmall: true,
+      price: 6999
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-photo/view-3d-camera_23-2151067031.jpg?t=st=1725170816~exp=1725174416~hmac=ebcb4f4add75f14c8a620f96c10646dd5dd878268ced869314897c3b6581c038&w=996",
+      title: "Video Camera",
+      description: "Panasonic AG-CX AG-CX10 4K Camcorder Camcorder.",
+      isSmall: false,
+      price: 60000
+    },
+    {
+      imgUrl: "https://img.freepik.com/free-vector/powerbank-battery-charger-realistic-icons-set-with-black-silver-devices-isolated-vector-illustration_1284-81780.jpg?t=st=1725170600~exp=1725174200~hmac=cf884fcd39f3a003304b638a3a170ee04a85da1ffa625d7cbe7c5af1bfa3ddcb&w=996",
+      title: "Power Bank",
+      description: "Mi 10000 mAh 33 W Power Bank (Lithium-ion, Power Delivery 3.0 for devices).",
+      isSmall: true,
+      price: 1999
+    }
+]
+
+  export{
+    PRODUCTS,
+    MAIN_TITLE,
+    PRODUCT_LIST
+  }
